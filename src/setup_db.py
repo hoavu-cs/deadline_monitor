374950996187d46f500e06d0a21fdfb2d93edf04
Hoa Vu <hvutrong@gmail.com>
2025-08-06 03:28:54 -0700

    Add tags for task table

--- src/setup_db.py
+++ src/setup_db.py
@@ -9,7 +9,7 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS people (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    email TEXT
+    email TEXT UNIQUE NOT NULL
 )
 """)
 
@@ -18,8 +18,7 @@ CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     description TEXT,
-    deadline TEXT
-)
+    deadline TEXT)
 """)
 
 cursor.execute("""
@@ -27,7 +26,7 @@ CREATE TABLE IF NOT EXISTS task_assignments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     person_id INTEGER,
     task_id INTEGER,
-    role TEXT CHECK(role IN ('supervisor', 'implementer')),
+    role TEXT CHECK(role IN ('supervisor', 'member')),
     FOREIGN KEY (person_id) REFERENCES people(id),
     FOREIGN KEY (task_id) REFERENCES tasks(id)
 )
